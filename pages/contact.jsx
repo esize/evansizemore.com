7e723d80520b438338730fb6e13ba2dc744619d3
Evan Sizemore <esize2004@gmail.com>
2021-07-05 14:33:44 -0400

    added page title

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -11,6 +11,7 @@ import {
 } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
+import Head from 'next/head';
 
 export default function Contact() {
   const {
@@ -46,89 +47,94 @@ export default function Contact() {
   }
 
   return (
-    <Container maxW='container.xl' mt={['14', '15', '48']}>
-      <Heading as='h1' fontSize={['7xl', '7xl', '8xl']} mb={['5', '8', '10']}>
-        Contact
-      </Heading>
+    <div>
+      <Head>
+        <title>Contact | Evan Sizemore</title>
+      </Head>
+      <Container maxW='container.xl' mt={['14', '15', '48']}>
+        <Heading as='h1' fontSize={['7xl', '7xl', '8xl']} mb={['5', '8', '10']}>
+          Contact
+        </Heading>
 
-      <form onSubmit={handleSubmit(onSubmitForm)}>
-        <FormControl isInvalid={errors.name} mb='5'>
-          <FormLabel htmlFor='name' fontSize='lg' fontWeight='bold'>
-            Name
-          </FormLabel>
-          <Input
-            id='name'
-            placeholder='Full Name'
-            {...register('name', { required: 'You must enter your name' })}
-          />
-          <FormErrorMessage>
-            {errors.name && errors.name.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={errors.email} mb='5'>
-          <FormLabel htmlFor='email' fontSize='lg' fontWeight='bold'>
-            Email
-          </FormLabel>
-          <Input
-            id='email'
-            placeholder='Email'
-            {...register('email', {
-              required: 'You must enter an email',
-              minLength: {
-                value: 8,
-                message: 'This is not long enough to be an email',
-              },
-              maxLength: {
-                value: 120,
-                message: 'This is too long',
-              },
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'This needs to be a valid email address',
-              },
-            })}
-          />
-          <FormErrorMessage>
-            {errors.email && errors.email.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={errors.message}>
-          <FormLabel htmlFor='message' fontSize='lg' fontWeight='bold'>
-            Message
-          </FormLabel>
-          <Textarea
-            id='message'
-            size='lg'
-            placeholder='Write your message here...'
-            {...register('message', {
-              required: 'You must enter a message',
-              maxLength: {
-                value: 1000,
-                message: "Your message can't be more than 1000 characters",
-              },
-              minLength: {
-                value: 50,
-                message: 'Your message must be longer than this!',
-              },
-            })}
-          ></Textarea>
-          <FormErrorMessage>
-            {errors.message && errors.message.message}
-          </FormErrorMessage>
-        </FormControl>
-        <Button
-          mt={4}
-          isLoading={isSubmitting}
-          type='submit'
-          fontSize='xl'
-          fontWeight='bold'
-          colorScheme='mojo'
-          paddingX='10'
-          paddingY='8'
-        >
-          Submit
-        </Button>
-      </form>
-    </Container>
+        <form onSubmit={handleSubmit(onSubmitForm)}>
+          <FormControl isInvalid={errors.name} mb='5'>
+            <FormLabel htmlFor='name' fontSize='lg' fontWeight='bold'>
+              Name
+            </FormLabel>
+            <Input
+              id='name'
+              placeholder='Full Name'
+              {...register('name', { required: 'You must enter your name' })}
+            />
+            <FormErrorMessage>
+              {errors.name && errors.name.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.email} mb='5'>
+            <FormLabel htmlFor='email' fontSize='lg' fontWeight='bold'>
+              Email
+            </FormLabel>
+            <Input
+              id='email'
+              placeholder='Email'
+              {...register('email', {
+                required: 'You must enter an email',
+                minLength: {
+                  value: 8,
+                  message: 'This is not long enough to be an email',
+                },
+                maxLength: {
+                  value: 120,
+                  message: 'This is too long',
+                },
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'This needs to be a valid email address',
+                },
+              })}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.message}>
+            <FormLabel htmlFor='message' fontSize='lg' fontWeight='bold'>
+              Message
+            </FormLabel>
+            <Textarea
+              id='message'
+              size='lg'
+              placeholder='Write your message here...'
+              {...register('message', {
+                required: 'You must enter a message',
+                maxLength: {
+                  value: 1000,
+                  message: "Your message can't be more than 1000 characters",
+                },
+                minLength: {
+                  value: 50,
+                  message: 'Your message must be longer than this!',
+                },
+              })}
+            ></Textarea>
+            <FormErrorMessage>
+              {errors.message && errors.message.message}
+            </FormErrorMessage>
+          </FormControl>
+          <Button
+            mt={4}
+            isLoading={isSubmitting}
+            type='submit'
+            fontSize='xl'
+            fontWeight='bold'
+            colorScheme='mojo'
+            paddingX='10'
+            paddingY='8'
+          >
+            Submit
+          </Button>
+        </form>
+      </Container>
+    </div>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -13,32 +13,170 @@ import {
 } from '@chakra-ui/react';
 
 import Image from 'next/image';
+import Head from 'next/head';
 import Link from 'next/link';
 import AngledContainer from '../components/AngledContainer';
 
 export default function Home() {
   return (
-    <Box overflow='hidden'>
-      <Container maxW='container.xl' mt={['14', '15', '48']}>
-        <Flex direction={['column', 'column', 'row']}>
-          <Box flex='1'>
-            <Heading
-              as='h1'
-              fontSize={['7xl', '7xl', '8xl']}
-              mb={['5', '8', '10']}
-              ml={['10', '10', '0']}
-            >
-              Hi, I'm Evan!
-            </Heading>
+    <div>
+      <Head>
+        <title>Home | Evan Sizemore</title>
+      </Head>
+      <Box overflow='hidden'>
+        <Container maxW='container.xl' mt={['14', '15', '48']}>
+          <Flex direction={['column', 'column', 'row']}>
+            <Box flex='1'>
+              <Heading
+                as='h1'
+                fontSize={['7xl', '7xl', '8xl']}
+                mb={['5', '8', '10']}
+                ml={['10', '10', '0']}
+              >
+                Hi, I'm Evan!
+              </Heading>
 
-            <Text
-              fontSize={['4xl', '4xl', '5xl']}
-              mb='8'
-              ml={['10', '10', '0']}
+              <Text
+                fontSize={['4xl', '4xl', '5xl']}
+                mb='8'
+                ml={['10', '10', '0']}
+              >
+                I’ll help you take control of your online presence.
+              </Text>
+              <Flex justifyContent={['center', 'center', 'left']}>
+                <Link href='/contact'>
+                  <Button
+                    fontSize='xl'
+                    fontWeight='bold'
+                    colorScheme='mojo'
+                    paddingX='10'
+                    paddingY='8'
+                  >
+                    Let's Talk
+                  </Button>
+                </Link>
+              </Flex>
+            </Box>
+            <Center
+              ml={['20', '20', '10']}
+              mr={['20', '20', '0']}
+              mt={['16', '16', '0']}
             >
-              I’ll help you take control of your online presence.
-            </Text>
-            <Flex justifyContent={['center', 'center', 'left']}>
+              <Image
+                src='/evan.jpg'
+                width={350}
+                height={350}
+                className='roundedImage'
+              />
+            </Center>
+          </Flex>
+        </Container>
+        <Box width='100vw' backgroundColor='#963038'>
+          <Box
+            backgroundColor='#1B1739'
+            width='100vw'
+            mt='20'
+            pt='20'
+            clipPath='polygon(
+            0 0,
+            100% 0,
+            100% calc(100% - 10vw),
+            0 100%
+          );'
+          >
+            <Container maxW='container.xl' pb={['28', '28', '64']}>
+              <Flex direction={['column', 'column', 'row']}>
+                <Box mr={['10', '10', '20']} ml={['10', '10', '0']}>
+                  <Heading
+                    as='h2'
+                    textDecoration='underline'
+                    textDecorationColor='brand.500'
+                    fontSize='5xl'
+                  >
+                    You need a digital brand.
+                  </Heading>
+
+                  <UnorderedList mt='12'>
+                    <ListItem fontSize='2xl' pb='4'>
+                      Have you Googled your name lately? The search results for
+                      your name are more important than ever when it comes to
+                      being cast, since the pandemic has made directors
+                      hyperfocused on your digital presence.
+                    </ListItem>
+                    <ListItem fontSize='2xl' pb='4'>
+                      In masterclasses, Broadway stars often preach the
+                      importance of defining your digital brand, since it is
+                      vital to your success as a performer.
+                    </ListItem>
+                    <ListItem fontSize='2xl' pb='4'>
+                      Owning your home on the internet allows you to have
+                      complete control over all aspects of your brand, making
+                      you appear like the professional you are.
+                    </ListItem>
+                    <ListItem fontSize='2xl' pb='4'>
+                      Directors are inevitably going to search you up. Do you
+                      want the first thing they see to be your childhood YouTube
+                      channel? Digital branding ensures that they only see the
+                      best of you.
+                    </ListItem>
+                  </UnorderedList>
+                </Box>
+                <Box
+                  ml={['10', '10', '20']}
+                  mr={['10', '10', '0']}
+                  mt={['10', '10', '0']}
+                >
+                  <Heading
+                    as='h2'
+                    textDecoration='underline'
+                    textDecorationColor='brand.500'
+                    fontSize='5xl'
+                  >
+                    Why work with me?
+                  </Heading>
+
+                  <UnorderedList mt='12'>
+                    <ListItem fontSize='2xl' pb='4'>
+                      As a former actor myself, I’ve had the privilege of
+                      working with both Broadway stars and successful casting
+                      directors in my role as a member of the leadership team
+                      for the Florida State Thespian Festival.
+                    </ListItem>
+                    <ListItem fontSize='2xl' pb='4'>
+                      Through my experience with them, I know what casting
+                      directors are looking for in an actor. Digital presence is
+                      a make or break factor when it comes to getting your dream
+                      roles.
+                    </ListItem>
+                    <ListItem fontSize='2xl' pb='4'>
+                      Combined with 10 years of technological expertise, this
+                      insider knowledge allows me to build a digital presence
+                      that will book you roles.
+                    </ListItem>
+                  </UnorderedList>
+                </Box>
+              </Flex>
+            </Container>
+          </Box>
+        </Box>
+        <AngledContainer />
+        <Box backgroundColor='#3D3481'>
+          <Container
+            maxWidth='container.md'
+            paddingY='36'
+            pr={['10', '10', '20']}
+            pl={['10', '10', '0']}
+          >
+            <Center>
+              <Heading
+                as='h2'
+                fontSize={['6xl', '6xl', '7xl']}
+                textAlign='center'
+              >
+                <i>Ready to get started?</i>
+              </Heading>
+            </Center>
+            <Center mt='12'>
               <Link href='/contact'>
                 <Button
                   fontSize='xl'
@@ -50,142 +188,10 @@ export default function Home() {
                   Let's Talk
                 </Button>
               </Link>
-            </Flex>
-          </Box>
-          <Center
-            ml={['20', '20', '10']}
-            mr={['20', '20', '0']}
-            mt={['16', '16', '0']}
-          >
-            <Image
-              src='/evan.jpg'
-              width={350}
-              height={350}
-              className='roundedImage'
-            />
-          </Center>
-        </Flex>
-      </Container>
-      <Box width='100vw' backgroundColor='#963038'>
-        <Box
-          backgroundColor='#1B1739'
-          width='100vw'
-          mt='20'
-          pt='20'
-          clipPath='polygon(
-            0 0,
-            100% 0,
-            100% calc(100% - 10vw),
-            0 100%
-          );'
-        >
-          <Container maxW='container.xl' pb={['28', '28', '64']}>
-            <Flex direction={['column', 'column', 'row']}>
-              <Box mr={['10', '10', '20']} ml={['10', '10', '0']}>
-                <Heading
-                  as='h2'
-                  textDecoration='underline'
-                  textDecorationColor='brand.500'
-                  fontSize='5xl'
-                >
-                  You need a digital brand.
-                </Heading>
-
-                <UnorderedList mt='12'>
-                  <ListItem fontSize='2xl' pb='4'>
-                    Have you Googled your name lately? The search results for
-                    your name are more important than ever when it comes to
-                    being cast, since the pandemic has made directors
-                    hyperfocused on your digital presence.
-                  </ListItem>
-                  <ListItem fontSize='2xl' pb='4'>
-                    In masterclasses, Broadway stars often preach the importance
-                    of defining your digital brand, since it is vital to your
-                    success as a performer.
-                  </ListItem>
-                  <ListItem fontSize='2xl' pb='4'>
-                    Owning your home on the internet allows you to have complete
-                    control over all aspects of your brand, making you appear
-                    like the professional you are.
-                  </ListItem>
-                  <ListItem fontSize='2xl' pb='4'>
-                    Directors are inevitably going to search you up. Do you want
-                    the first thing they see to be your childhood YouTube
-                    channel? Digital branding ensures that they only see the
-                    best of you.
-                  </ListItem>
-                </UnorderedList>
-              </Box>
-              <Box
-                ml={['10', '10', '20']}
-                mr={['10', '10', '0']}
-                mt={['10', '10', '0']}
-              >
-                <Heading
-                  as='h2'
-                  textDecoration='underline'
-                  textDecorationColor='brand.500'
-                  fontSize='5xl'
-                >
-                  Why work with me?
-                </Heading>
-
-                <UnorderedList mt='12'>
-                  <ListItem fontSize='2xl' pb='4'>
-                    As a former actor myself, I’ve had the privilege of working
-                    with both Broadway stars and successful casting directors in
-                    my role as a member of the leadership team for the Florida
-                    State Thespian Festival.
-                  </ListItem>
-                  <ListItem fontSize='2xl' pb='4'>
-                    Through my experience with them, I know what casting
-                    directors are looking for in an actor. Digital presence is a
-                    make or break factor when it comes to getting your dream
-                    roles.
-                  </ListItem>
-                  <ListItem fontSize='2xl' pb='4'>
-                    Combined with 10 years of technological expertise, this
-                    insider knowledge allows me to build a digital presence that
-                    will book you roles.
-                  </ListItem>
-                </UnorderedList>
-              </Box>
-            </Flex>
+            </Center>
           </Container>
         </Box>
       </Box>
-      <AngledContainer />
-      <Box backgroundColor='#3D3481'>
-        <Container
-          maxWidth='container.md'
-          paddingY='36'
-          pr={['10', '10', '20']}
-          pl={['10', '10', '0']}
-        >
-          <Center>
-            <Heading
-              as='h2'
-              fontSize={['6xl', '6xl', '7xl']}
-              textAlign='center'
-            >
-              <i>Ready to get started?</i>
-            </Heading>
-          </Center>
-          <Center mt='12'>
-            <Link href='/contact'>
-              <Button
-                fontSize='xl'
-                fontWeight='bold'
-                colorScheme='mojo'
-                paddingX='10'
-                paddingY='8'
-              >
-                Let's Talk
-              </Button>
-            </Link>
-          </Center>
-        </Container>
-      </Box>
-    </Box>
+    </div>
   );
 }
